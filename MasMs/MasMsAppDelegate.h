8fa587d7af751aaf3763da2eeb30405caa9aa566
hpyhacking <crazyflapjack@gmail.com>
2012-12-24 16:33:25 +0800

    Initial Commit

---
+++ MasMs/MasMsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MasMsAppDelegate.h
+//  MasMs
+//
+//  Created by Jack Qiu on 12/24/12.
+//  Copyright (c) 2012 hpyhacking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasMsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MasMs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MasMs
+//
+//  Created by Jack Qiu on 12/24/12.
+//  Copyright (c) 2012 hpyhacking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MasMsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MasMsAppDelegate class]));
+  }
+}
