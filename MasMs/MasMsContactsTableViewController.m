c01fc8454153cf5d06e3ea8aa3c1eed2724007e5
hpyhacking <crazyflapjack@gmail.com>
2012-12-27 00:55:00 +0800

    change load contact function, use progress view show when sending message to replace title view.

--- MasMs/MasMsContactsTableViewController.m
+++ MasMs/MasMsContactsTableViewController.m
@@ -11,11 +11,28 @@
 #import <AddressBook/AddressBook.h>
 
 @interface MasMsContactsTableViewController ()
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *sendButton;
+
+@property (nonatomic) NSInteger checkedCount;
+@property (nonatomic) BOOL isSending;
 @property (nonatomic, strong) NSArray *groups;
 @property (nonatomic, strong) NSArray *people;
 @end
 
 @implementation MasMsContactsTableViewController
+
+- (UIProgressView *) setProgress {
+    UIProgressView *progress = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
+    progress.frame = CGRectMake(0, 30-progress.frame.size.height, 200, progress.frame.size.height);
+    self.navigationItem.titleView = progress;
+    return progress;
+}
+
+- (void) setSpinner {
+    UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    [spinner startAnimating];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:spinner];
+}
 
 - (void) loadAddressBook {
     // 生成索引顺序相同的两个数组来存放Group (NSString) 与People (MasMsContact)
@@ -23,38 +40,43 @@
     
     ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
     
-    NSMutableArray *groups = [NSMutableArray arrayWithCapacity:ABAddressBookGetGroupCount(addressBook)];
-    NSMutableArray *people = [NSMutableArray arrayWithCapacity:ABAddressBookGetGroupCount(addressBook)];
+    NSMutableArray *groups = [NSMutableArray arrayWithCapacity:(1 + ABAddressBookGetGroupCount(addressBook))];
+    NSMutableArray *people = [NSMutableArray arrayWithCapacity:(1 + ABAddressBookGetGroupCount(addressBook))];
     
     [(NSArray *)CFBridgingRelease(ABAddressBookCopyArrayOfAllGroups(addressBook)) enumerateObjectsUsingBlock:^(id obj_g, NSUInteger idx, BOOL *stop) {
         ABRecordRef g = (__bridge ABRecordRef)obj_g;
-        NSString *gname = CFBridgingRelease(ABRecordCopyCompositeName(g));
+        [groups addObject:CFBridgingRelease(ABRecordCopyCompositeName(g))];
+        [people addObject:[[[NSArray alloc] init] mutableCopy]];
+    }];
+    
+    NSString *ungrouped = NSLocalizedString(@"UNGROUPED", NULL);
+    
+    [groups addObject:ungrouped];
+    [people addObject:[[[NSArray alloc] init] mutableCopy]];
+    
+    [(NSArray *)CFBridgingRelease(ABAddressBookCopyArrayOfAllPeople(addressBook)) enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        ABRecordRef ref = (__bridge ABRecordRef)obj;
+        ABRecordRef group_ref = ABAddressBookGetGroupWithRecordID(addressBook, ABRecordGetRecordID(ref));
+        ABMultiValueRef numbers_ref = ABRecordCopyValue(ref, kABPersonPhoneProperty);
         
-        NSMutableArray *gp = [[[NSArray alloc] init] mutableCopy];
+        NSString *pname = CFBridgingRelease(ABRecordCopyCompositeName(ref));
+        NSString *gname = (group_ref == NULL) ? ungrouped : CFBridgingRelease(ABRecordCopyCompositeName(group_ref));
         
-        [(NSArray *)CFBridgingRelease(ABGroupCopyArrayOfAllMembers(g)) enumerateObjectsUsingBlock:^(id obj_p, NSUInteger idx, BOOL *stop) {
-            ABRecordRef p = (__bridge ABRecordRef)obj_p;
-            ABMultiValueRef numbers = ABRecordCopyValue(p, kABPersonPhoneProperty);
+        NSMutableArray *gpeople = [people objectAtIndex:[groups indexOfObject:gname]];
+        
+        [(NSArray *) CFBridgingRelease(ABMultiValueCopyArrayOfAllValues(numbers_ref)) enumerateObjectsUsingBlock:^(id num, NSUInteger idx, BOOL *stop) {
+            NSString *pnumber = CFBridgingRelease(CFBridgingRetain(num));
             
-            NSString *pname = CFBridgingRelease(ABRecordCopyCompositeName(p));
+            MasMsContact *new_c = [[MasMsContact alloc] init];
+            new_c.name = pname;
+            new_c.number = pnumber;
+            new_c.group = gname;
+            new_c.checked = NO;
             
-            [(NSArray *) CFBridgingRelease(ABMultiValueCopyArrayOfAllValues(numbers)) enumerateObjectsUsingBlock:^(id obj_n, NSUInteger idx, BOOL *stop) {
-                NSString *pnumber = CFBridgingRelease(CFBridgingRetain(obj_n));
-                
-                MasMsContact *new_c = [[MasMsContact alloc] init];
-                new_c.name = pname;
-                new_c.number = pnumber;
-                new_c.group = gname;
-                new_c.checked = YES;
-                
-                [gp addObject:new_c];
-            }];
-            
-            CFRelease(numbers);
+            [gpeople addObject:new_c];
         }];
         
-        [people addObject:[gp copy]];
-        [groups addObject:gname];
+        CFRelease(numbers_ref);
     }];
     
     CFRelease(addressBook);
@@ -64,9 +86,55 @@
 }
 
 - (void)viewDidLoad {
+    self.isSending = NO;
+    
     [super viewDidLoad];
     [self loadAddressBook];
+    [self setCheckCounter:0];
 }
+
+- (void)setCheckCounter:(NSInteger)count {
+    if (count == 0) self.checkedCount = 0;
+    if (count != 0) self.checkedCount = self.checkedCount + count;
+    self.sendButton.enabled = (self.checkedCount > 0);
+}
+
+- (IBAction)send:(UIBarButtonItem *)sender {
+    self.isSending = YES;
+    
+    [self setSpinner];
+    
+    UIView *originLabel = self.navigationItem.titleView;
+    UIProgressView *progress = [self setProgress];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSMutableArray *col = [[NSMutableArray alloc] init];
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"checked == YES"];
+        
+        for (NSArray *array in self.people) {
+            [col addObjectsFromArray:[array filteredArrayUsingPredicate:predicate]];
+        }
+        
+        [col enumerateObjectsUsingBlock:^(MasMsContact *obj, NSUInteger idx, BOOL *stop) {
+            NSLog(@"simulator send message to %@", obj.name);
+            
+            float rate = [[NSNumber numberWithUnsignedInteger:(idx + 1)] floatValue] / col.count;
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [progress setProgress:rate animated:YES];
+            });
+            
+            sleep(1.5);
+        }];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.isSending = NO;
+            self.navigationItem.rightBarButtonItem = sender;
+            self.navigationItem.titleView = originLabel;
+        });
+    });
+}
+
 
 #pragma mark - TableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -91,17 +159,20 @@
 
 #pragma mark - TableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    MasMsContact *contac = [[self.people objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
-    contac.checked = !contac.checked;
-    
-    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-    cell.accessoryType = contac.checked ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    if (self.isSending == NO) {
+        MasMsContact *contac = [[self.people objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+        contac.checked = !contac.checked;
+        
+        UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+        cell.accessoryType = contac.checked ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+        
+        [self setCheckCounter:contac.checked == YES ? 1 : -1];
+    }
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
-- (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-{
+- (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
     UIView *view = [[UIView alloc] init];
     
     CGRect rect = CGRectMake(5, 0, 300, 30);
--- MasMs/MasMsSMSTableViewController.m
+++ MasMs/MasMsSMSTableViewController.m
@@ -68,6 +68,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
